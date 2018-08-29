1d00ace3645248ded0e26cb44601a061b1af1e62
Dcrew <dean.reynolds@live.co.uk>
2018-08-29 17:51:09 +0100

    Cleanup

--- Tile Engine/Scenes/Game.cs
+++ Tile Engine/Scenes/Game.cs
@@ -1,5 +1,4 @@
-﻿using Tile_Engine.Entities;
-using Microsoft.Xna.Framework;
+﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
@@ -9,21 +8,15 @@ namespace Tile_Engine.Scenes
     {
         private readonly World _world;
         private readonly Camera _camera;
-        private readonly Player[] _players;
 
         public Game(int maxPlayers)
         {
             _world = World.Generate(400, 400);
             _camera = new Camera(new Vector2((_world.Spawn.X * Tile.Size), (_world.Spawn.Y * Tile.Size)));
-            _players = new Player[maxPlayers];
-            _players[0] = new Player();
         }
 
         public override void Update(GameTime gameTime)
         {
-            for (int i = 0; i < _players.Length; i++)
-                if (_players[i] != null)
-                    _players[i].Update(gameTime);
             float camSpeed = (float)(500 * gameTime.ElapsedGameTime.TotalSeconds);
             if (Keyboard.GetState().IsKeyDown(Keys.W))
                 _camera.Y -= camSpeed;
@@ -45,10 +38,6 @@ namespace Tile_Engine.Scenes
         {
             spriteBatch.Begin(SpriteSortMode.Deferred, null, SamplerState.PointClamp, null, null, null, null);
             _world.Draw(spriteBatch, gameTime);
-            spriteBatch.End();
-            spriteBatch.Begin(SpriteSortMode.Deferred, null, SamplerState.PointClamp, null, null, null, _camera.Transform);
-            foreach (Player player in _players)
-                player?.Draw(gameTime);
             spriteBatch.End();
             base.Draw(spriteBatch, gameTime);
         }
