e649a22ac6164c357146c94283d70620f707c411
Dcrew <dean.reynolds@live.co.uk>
2018-10-02 19:30:17 +0100

    Updated Camera, removed several unnecessary matrices

--- Tile Engine/Camera.cs
+++ Tile Engine/Camera.cs
@@ -10,8 +10,8 @@ namespace Tile_Engine
             get { return _position.X; }
             set
             {
-                _positionTranslation.M41 = -(_position.X = value);
-                UpdatePositionInTransform();
+                _position.X = value;
+                UpdateXPositionInTransform();
             }
         }
         public float Y
@@ -19,8 +19,8 @@ namespace Tile_Engine
             get { return _position.Y; }
             set
             {
-                _positionTranslation.M42 = -(_position.Y = value);
-                UpdatePositionInTransform();
+                _position.Y = value;
+                UpdateYPositionInTransform();
             }
         }
         public Vector2 Position
@@ -28,8 +28,7 @@ namespace Tile_Engine
             get { return _position; }
             set
             {
-                _positionTranslation.M41 = -(_position.X = value.X);
-                _positionTranslation.M42 = -(_position.Y = value.Y);
+                _position = value;
                 UpdatePositionInTransform();
             }
         }
@@ -38,18 +37,18 @@ namespace Tile_Engine
             get { return _angle; }
             set
             {
-                _rotationZ.M22 = _rotationZ.M11 = (float)System.Math.Cos(-(_angle = value));
-                _rotationZ.M21 = -(_rotationZ.M12 = (float)System.Math.Sin(-_angle));
+                _rotM11 = (float)System.Math.Cos(-(_angle = value));
+                _rotM12 = (float)System.Math.Sin(-_angle);
                 UpdateScaleInTransform();
                 UpdatePositionInTransform();
             }
         }
         public float Scale
         {
-            get { return _scale.M11; }
+            get { return _scale; }
             set
             {
-                _scale.M11 = _scale.M22 = value;
+                _scale = value;
                 UpdateScaleInTransform();
                 UpdatePositionInTransform();
             }
@@ -60,13 +59,13 @@ namespace Tile_Engine
             set
             {
                 _screenSize = value;
+                _screenCenter.X = (_screenSize.X / 2f);
+                _screenCenter.Y = (_screenSize.Y / 2f);
+                UpdatePositionInTransform();
                 _projection.M11 = (float)(2d / _screenSize.X);
                 _projection.M22 = (float)(2d / -_screenSize.Y);
                 _projection.M41 = (float)((double)_screenSize.X / -_screenSize.X);
                 _projection.M42 = (float)((double)_screenSize.Y / _screenSize.Y);
-                _screenTranslation.M41 = (_screenSize.X / 2f);
-                _screenTranslation.M42 = (_screenSize.Y / 2f);
-                UpdatePositionInTransform();
             }
         }
 
@@ -77,11 +76,15 @@ namespace Tile_Engine
         private Vector2 _position;
         private float _angle;
         private Vector2 _screenSize;
-        private Matrix _positionTranslation;
-        private Matrix _rotationZ;
-        private Matrix _scale;
+        private Vector2 _screenCenter;
+        private float _rotM11;
+        private float _rotM12;
+        private float _rotX1;
+        private float _rotY1;
+        private float _rotX2;
+        private float _rotY2;
+        private float _scale;
         private double _n27;
-        private Matrix _screenTranslation;
         private Matrix _transform;
         private Matrix _transformInvert;
         private Vector2 _mousePosition;
@@ -90,43 +93,14 @@ namespace Tile_Engine
         public Camera(float angle = 0, float scale = 1) : this(Vector2.Zero, angle, scale) { }
         public Camera(Vector2 position, float angle = 0, float scale = 1)
         {
-            _positionTranslation = new Matrix
-            {
-                M11 = 1,
-                M22 = 1,
-                M33 = 1,
-                M41 = -(_position.X = position.X),
-                M42 = -(_position.Y = position.Y),
-                M44 = 1
-            };
-            float rotVal1 = (float)System.Math.Cos(-(_angle = angle));
-            float rotVal2 = (float)System.Math.Sin(-_angle);
-            _rotationZ = new Matrix
-            {
-                M11 = rotVal1,
-                M12 = rotVal2,
-                M21 = -rotVal2,
-                M22 = rotVal1,
-                M33 = 1,
-                M44 = 1
-            };
-            _scale = new Matrix
-            {
-                M11 = scale,
-                M22 = scale,
-                M33 = 1,
-                M44 = 1
-            };
+            _position.X = position.X;
+            _position.Y = position.Y;
+            _rotM11 = (float)System.Math.Cos(-(_angle = angle));
+            _rotM12 = (float)System.Math.Sin(-_angle);
+            _scale = scale;
             _screenSize = new Vector2(Game1.Viewport.Width, Game1.Viewport.Height);
-            _screenTranslation = new Matrix
-            {
-                M11 = 1,
-                M22 = 1,
-                M33 = 1,
-                M41 = (_screenSize.X / 2),
-                M42 = (_screenSize.Y / 2),
-                M44 = 1
-            };
+            _screenCenter.X = (_screenSize.X / 2f);
+            _screenCenter.Y = (_screenSize.Y / 2f);
             _transform = new Matrix
             {
                 M33 = 1,
@@ -160,22 +134,48 @@ namespace Tile_Engine
             _mousePosition.Y = ((mouseX * _transformInvert.M12) + (mouseY * _transformInvert.M22) + _transformInvert.M42);
         }
 
+        private void UpdateXPositionInTransform()
+        {
+            float m41 = (-_position.X * _scale);
+            _rotX1 = (m41 * _rotM11);
+            _rotX2 = (m41 * _rotM12);
+            _transform.M41 = ((_rotX1 + _rotY1) + _screenCenter.X);
+            _transform.M42 = ((_rotX2 + _rotY2) + _screenCenter.Y);
+            _transformInvert.M41 = (float)(-((double)_transform.M21 * -_transform.M42 - (double)_transform.M22 * -_transform.M41) * _n27);
+            _transformInvert.M42 = (float)(((double)_transform.M11 * -_transform.M42 - (double)_transform.M12 * -_transform.M41) * _n27);
+        }
+
+        private void UpdateYPositionInTransform()
+        {
+            float m42 = (-_position.Y * _scale);
+            _rotY1 = (m42 * -_rotM12);
+            _rotY2 = (m42 * _rotM11);
+            _transform.M41 = ((_rotX1 + _rotY1) + _screenCenter.X);
+            _transform.M42 = ((_rotX2 + _rotY2) + _screenCenter.Y);
+            _transformInvert.M41 = (float)(-((double)_transform.M21 * -_transform.M42 - (double)_transform.M22 * -_transform.M41) * _n27);
+            _transformInvert.M42 = (float)(((double)_transform.M11 * -_transform.M42 - (double)_transform.M12 * -_transform.M41) * _n27);
+        }
+
         private void UpdatePositionInTransform()
         {
-            float m41 = (_positionTranslation.M41 * _scale.M11);
-            float m42 = (_positionTranslation.M42 * _scale.M22);
-            _transform.M41 = (((m41 * _rotationZ.M11) + (m42 * _rotationZ.M21)) + _screenTranslation.M41);
-            _transform.M42 = (((m41 * _rotationZ.M12) + (m42 * _rotationZ.M22)) + _screenTranslation.M42);
+            float m41 = (-_position.X * _scale);
+            float m42 = (-_position.Y * _scale);
+            _rotX1 = (m41 * _rotM11);
+            _rotY1 = (m42 * -_rotM12);
+            _rotX2 = (m41 * _rotM12);
+            _rotY2 = (m42 * _rotM11);
+            _transform.M41 = ((_rotX1 + _rotY1) + _screenCenter.X);
+            _transform.M42 = ((_rotX2 + _rotY2) + _screenCenter.Y);
             _transformInvert.M41 = (float)(-((double)_transform.M21 * -_transform.M42 - (double)_transform.M22 * -_transform.M41) * _n27);
             _transformInvert.M42 = (float)(((double)_transform.M11 * -_transform.M42 - (double)_transform.M12 * -_transform.M41) * _n27);
         }
 
         public void UpdateScaleInTransform()
         {
-            _transform.M11 = ((_scale.M11 * _rotationZ.M11) + (_scale.M12 * _rotationZ.M21));
-            _transform.M12 = ((_scale.M11 * _rotationZ.M12) + (_scale.M12 * _rotationZ.M22));
-            _transform.M21 = ((_scale.M21 * _rotationZ.M11) + (_scale.M22 * _rotationZ.M21));
-            _transform.M22 = ((_scale.M21 * _rotationZ.M12) + (_scale.M22 * _rotationZ.M22));
+            _transform.M11 = (_scale * _rotM11);
+            _transform.M12 = (_scale * _rotM12);
+            _transform.M21 = (_scale * -_rotM12);
+            _transform.M22 = (_scale * _rotM11);
             _n27 = (1d / ((double)_transform.M11 * _transform.M22 + (double)_transform.M12 * -_transform.M21));
             _transformInvert.M11 = (float)(_transform.M22 * _n27);
             _transformInvert.M21 = (float)(-_transform.M21 * _n27);
